b9c69370a7cd8c30f08ead9705abd0534a2015f0
htrsng <tranghuyen20051312@gmail.com>
2024-07-17 14:47:27 +0700

    vscode

---
+++ b2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<math.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int tuoi;
+    cout<<"nhap so tuoi cua hoc sinh:";cin>>tuoi;
+
+   do{
+    cout<<"nhap lai:";cin>>tuoi;
+   }
+   while(tuoi <16);
+}
